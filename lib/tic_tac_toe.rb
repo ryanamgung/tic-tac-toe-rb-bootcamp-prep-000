11a15170d6a22234b7181ed1b88b73d87140d3da
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-25 01:28:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,6 +108,6 @@ def play(board)
     if(won?(board))
       puts "Congratulations #{winner(board)}!"
     elsif draw?(board)
-      puts "The game is a draw."
+      puts "Cat's Game!"
     end
 end
